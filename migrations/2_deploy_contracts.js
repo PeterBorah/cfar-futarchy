10a663777b191801c90d977677a5f002788d6c94
Peter Borah <peterborah@gmail.com>
2017-05-20 16:53:06 -0700

    Allow withdrawal after end of market.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
-var Market = artifacts.require("./Market.sol");
+var Dao = artifacts.require("./dao.sol");
+var Oracle = artifacts.require("./oracle.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Market);
+  var oracle;
+
+  deployer.deploy(Oracle);
+  deployer.then(function() { return Oracle.deployed() }).
+    then(function(result) { oracle = result }).
+    then(function() { return deployer.deploy(Dao, oracle.address); });
 };
