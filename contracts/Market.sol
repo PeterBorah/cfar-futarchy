4c28de18a800ae35cf65226543d938b44cd39916
Peter Borah <peterborah@gmail.com>
2017-05-20 13:12:48 -0700

    Add sell orders and sort buy and sell orders

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -4,14 +4,107 @@ contract Market {
   uint[] public buyAmounts;
   uint[] public buyPrices;
   address[] public buyOriginators;
+  uint[] public sellAmounts;
+  uint[] public sellPrices;
+  address[] public sellOriginators;
 
   function buy(uint amount, uint price) payable {
-    buyAmounts.push(amount);
-    buyPrices.push(price);
-    buyOriginators.push(msg.sender); 
+    uint index = _getBuyIndex(price);
+    _addBuyPriceAtIndex(index, price);
+    _addBuyAmountAtIndex(index, amount);
+    _addBuyOriginatorAtIndex(index, msg.sender);
   }
 
   function allBuyOrders() returns(uint[], uint[], address[]) {
     return (buyAmounts, buyPrices, buyOriginators);
+  }
+
+  function _getBuyIndex(uint price) private returns(uint) {
+    uint i = 0;
+
+    for (i; i < buyPrices.length; i++) {
+      if (buyPrices[i] > price) {
+        return i;
+      }
+    }
+
+    return i;
+  }
+
+  function _addBuyPriceAtIndex(uint index, uint price) private {
+    buyPrices.length++;
+    for (uint i = buyPrices.length - 1; i > index; i--) {
+      buyPrices[i] = buyPrices[i-1]; 
+    }
+
+    buyPrices[index] = price;
+  }
+
+  function _addBuyAmountAtIndex(uint index, uint amount) private {
+    buyAmounts.length++;
+    for (uint i = buyAmounts.length - 1; i > index; i--) {
+      buyAmounts[i] = buyAmounts[i-1]; 
+    }
+
+    buyAmounts[index] = amount;
+  }
+
+  function _addBuyOriginatorAtIndex(uint index, address originator) private {
+    buyOriginators.length++;
+    for (uint i = buyOriginators.length - 1; i > index; i--) {
+      buyOriginators[i] = buyOriginators[i-1]; 
+    }
+
+    buyOriginators[index] = originator;
+  }
+
+  function sell(uint amount, uint price) payable {
+    uint index = _getSellIndex(price);
+    _addSellPriceAtIndex(index, price);
+    _addSellAmountAtIndex(index, amount);
+    _addSellOriginatorAtIndex(index, msg.sender);
+  }
+
+  function allSellOrders() returns(uint[], uint[], address[]) {
+    return (sellAmounts, sellPrices, sellOriginators);
+  }
+
+  function _getSellIndex(uint price) private returns(uint) {
+    uint i = 0;
+
+    for (i; i < sellPrices.length; i++) {
+      if (sellPrices[i] < price) {
+        return i;
+      }
+    }
+
+    return i;
+  }
+
+  function _addSellPriceAtIndex(uint index, uint price) private {
+    sellPrices.length++;
+    for (uint i = sellPrices.length - 1; i > index; i--) {
+      sellPrices[i] = sellPrices[i-1]; 
+    }
+
+    sellPrices[index] = price;
+  }
+
+  function _addSellAmountAtIndex(uint index, uint amount) private {
+    sellAmounts.length++;
+    for (uint i = sellAmounts.length - 1; i > index; i--) {
+      sellAmounts[i] = sellAmounts[i-1]; 
+    }
+
+    sellAmounts[index] = amount;
+  }
+
+  function _addSellOriginatorAtIndex(uint index, address originator) private {
+    sellOriginators.length++;
+    for (uint i = sellOriginators.length - 1; i > index; i--) {
+      sellOriginators[i] = sellOriginators[i-1]; 
+    }
+
+    sellOriginators[index] = originator;
   }
 }
--- test/market.js
+++ test/market.js
@@ -4,7 +4,7 @@ contract('Market', function(accounts) {
   it("should allow new buy orders", function() {
     var market;
 
-    return Market.deployed().then(function(instance) { market = instance }).
+    return Market.new().then(function(instance) { market = instance }).
       then(function() { return market.buy(10, 90, {value: 900}) }).
       then(function() { return market.buy(10, 95, {value: 950}) }).
       then(function() { return market.allBuyOrders.call() }).
@@ -17,6 +17,79 @@ contract('Market', function(accounts) {
         assert.equal(result[0][1], 10);
         assert.equal(result[1][1], 95);
         assert.equal(result[2][1], accounts[0]);
+      });
+  });
+
+  it("should order buy orders by price, low to high", function() {
+    var market;
+
+    return Market.new().then(function(instance) { market = instance }).
+      then(function() { return market.buy(10, 99, {value: 990}) }).
+      then(function() { return market.buy(10, 95, {value: 950}) }).
+      then(function() { return market.buy(10, 90, {value: 900}) }).
+      then(function() { return market.buy(10, 96, {value: 990}) }).
+      then(function() { return market.allBuyOrders.call() }).
+      then(function(result) {
+        assert.equal(result.length, 3);
+        assert.equal(result[0].length, 4);
+        assert.equal(result[0][0], 10);
+        assert.equal(result[1][0], 90);
+        assert.equal(result[2][0], accounts[0]);
+        assert.equal(result[0][1], 10);
+        assert.equal(result[1][1], 95);
+        assert.equal(result[2][1], accounts[0]);
+        assert.equal(result[0][2], 10);
+        assert.equal(result[1][2], 96);
+        assert.equal(result[2][2], accounts[0]);
+        assert.equal(result[0][3], 10);
+        assert.equal(result[1][3], 99);
+        assert.equal(result[2][3], accounts[0]);
+      });
+  });
+
+  it("should allow new sell orders", function() {
+    var market;
+
+    return Market.new().then(function(instance) { market = instance }).
+      then(function() { return market.sell(10, 35, {value: 950}) }).
+      then(function() { return market.sell(10, 30, {value: 900}) }).
+      then(function() { return market.allSellOrders.call() }).
+      then(function(result) {
+        assert.equal(result.length, 3);
+        assert.equal(result[0].length, 2);
+        assert.equal(result[0][0], 10);
+        assert.equal(result[1][0], 35);
+        assert.equal(result[2][0], accounts[0]);
+        assert.equal(result[0][1], 10);
+        assert.equal(result[1][1], 30);
+        assert.equal(result[2][1], accounts[0]);
+      });
+  });
+
+  it("should order sell orders by price, high to low", function() {
+    var market;
+
+    return Market.new().then(function(instance) { market = instance }).
+      then(function() { return market.sell(10, 36, {value: 990}) }).
+      then(function() { return market.sell(10, 39, {value: 990}) }).
+      then(function() { return market.sell(10, 30, {value: 900}) }).
+      then(function() { return market.sell(10, 35, {value: 950}) }).
+      then(function() { return market.allSellOrders.call() }).
+      then(function(result) {
+        assert.equal(result.length, 3);
+        assert.equal(result[0].length, 4);
+        assert.equal(result[0][0], 10);
+        assert.equal(result[1][0], 39);
+        assert.equal(result[2][0], accounts[0]);
+        assert.equal(result[0][1], 10);
+        assert.equal(result[1][1], 36);
+        assert.equal(result[2][1], accounts[0]);
+        assert.equal(result[0][2], 10);
+        assert.equal(result[1][2], 35);
+        assert.equal(result[2][2], accounts[0]);
+        assert.equal(result[0][3], 10);
+        assert.equal(result[1][3], 30);
+        assert.equal(result[2][3], accounts[0]);
       });
   });
 });
